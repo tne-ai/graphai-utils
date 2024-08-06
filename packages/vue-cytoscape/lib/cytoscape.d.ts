d32ce2040246aa70d9ecf8e369c38a86996741ba
isamu <isamu@to-kyo.to>
2024-08-06 20:31:36 +0900

    udpate

--- packages/vue-cytoscape/lib/cytoscape.d.ts
+++ packages/vue-cytoscape/lib/cytoscape.d.ts
@@ -1,7 +1,7 @@
 import { Ref, ComputedRef } from "vue";
 import { GraphData, NodeState } from "graphai";
 export declare const useCytoscape: (selectedGraph: ComputedRef<GraphData> | Ref<GraphData>) => {
-    cytoscapeRef: Ref<any>;
+    cytoscapeRef: Ref<any, any>;
     updateCytoscape: (nodeId: string, state: NodeState) => Promise<void>;
     resetCytoscape: () => void;
 };
