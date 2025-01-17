bc0ecd3663b961072cef7148cd4be6625dc1313d
isamu <isamu@to-kyo.to>
2025-01-17 20:00:11 +0900

    add files

---
+++ packages/vue-text-input-agent-generator/src/event_agent_generator.ts
@@ -0,0 +1,27 @@
+import { AgentFunction, AgentFunctionContext, agentInfoWrapper } from "graphai";
+
+export type EventData = { onEnd: (data: unknown) => void; id: string; nodeId: string; agentId?: string; type: string; params: any };
+
+export const eventAgentGenerator = (onStart: (id: string, data: EventData) => void) => {
+  const eventPromise = (context: AgentFunctionContext) => {
+    const id = Math.random().toString(32).substring(2);
+    return new Promise((resolved) => {
+      const onEnd = (data: unknown) => {
+        resolved(data);
+      };
+      const { params } = context;
+      const { nodeId, agentId } = context.debugInfo;
+      const { type } = params;
+      const data = { onEnd, id, nodeId, agentId, type, params };
+      onStart(id, data);
+    });
+  };
+
+  const eventAgent: AgentFunction = async (context) => {
+    const result = await eventPromise(context);
+    return result;
+  };
+  return {
+    eventAgent: agentInfoWrapper(eventAgent),
+  };
+};
---
+++ packages/vue-text-input-agent-generator/src/index.ts
@@ -0,0 +1,8 @@
+import { textInputAgentGenerator } from "./text_input_agent_generator";
+import type { InputEvents } from "./text_input_agent_generator";
+
+import { eventAgentGenerator } from "./event_agent_generator";
+import type { EventData } from "./event_agent_generator";
+
+export type { InputEvents, EventData };
+export { textInputAgentGenerator, eventAgentGenerator };
