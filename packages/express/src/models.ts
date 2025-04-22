c78d9ee15bd15b10c58f38ea93f5fc0457b7aa43
isamu <isamu@to-kyo.to>
2025-04-22 16:01:22 +0900

    fix

--- packages/express/src/models.ts
+++ packages/express/src/models.ts
@@ -4,7 +4,7 @@ import type { GraphDictionary } from "./type";
 export const modelList = (modelDictionary: GraphDictionary) => {
   return async (req: express.Request, res: express.Response) => {
     const data = Object.keys(modelDictionary).map((id) => {
-      const model = modelDictionary[id];
+      // const model = modelDictionary[id];
       return {
         id,
         object: "model",
